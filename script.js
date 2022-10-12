d4bc3976c161fadb1481b8fe0759fae747cb1938
Taby <forwardpivot@proton.me>
2022-10-11 20:38:34 -0500

    Added the animate function

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const ctx = canvas1.getContext('2d');
 canvas1.width = window.innerWidth;
 canvas1.height = window.innerHeight;
 
+
+/**
+ * This function redraws the canvas over, and over creating
+ * the illusion of movement
+ */
+function animate() {
 //changes the fill color to the value
 ctx.fillStyle = 'red';
+//Changes the color of the border
+ctx.strokeStyle='blue';
 //changes how thick the border is
-ctx.lineWidth = 2;
+ctx.lineWidth = 3;
 ctx.beginPath();
 /** CIRCLE
 *1st value moves it along the X axis
@@ -22,9 +30,15 @@ ctx.fill();
 //creates a border around it
 ctx.stroke();
 
+//Recursion
+requestAnimationFrame(animate);
+}
+animate();
+
+
 /** RECTANGLE
  * 1st two values move it on the X, and Y axis respectively
  * The last two are width, and height respectively
  */
 
-ctx.fillRect(100, 50, 100, 100);
+//ctx.fillRect(100, 50, 100, 100);
